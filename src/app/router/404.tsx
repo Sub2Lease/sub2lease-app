9b1a265df129b7cdb1196f138516c1ed633208f8
Adin Lieblein <adinmlieblein@gmail.com>
2026-03-07 17:15:45 -0600

    404 and default font color update

--- src/app/router/404.tsx
+++ src/app/router/404.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export function NotFoundRoute() {
 
   return (
@@ -6,6 +8,8 @@ export function NotFoundRoute() {
         <h1 className="mb-5 text-3xl lg:text-[40px] lg:leading-[40px]">
           Oops! <br /> Looks like you <br className="sm:hidden" /> got lost...
         </h1>
+
+        <Link className="mt-8 inline-block bg-white p-4 rounded-lg" to="/">Go back home</Link>
       </div>
      </div>
   );
