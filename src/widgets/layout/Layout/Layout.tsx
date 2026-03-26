42eaa353421abbe5147f4134c0f16f92905fb330
Anirudh <63116473+AnirudhJ77@users.noreply.github.com>
2026-03-26 17:34:14 -0500

    autoplay tweak

--- src/widgets/layout/Layout/Layout.tsx
+++ src/widgets/layout/Layout/Layout.tsx
@@ -34,6 +34,7 @@ export function Layout({ children }: PropsWithChildren) {
           ref={videoRef}
           muted
           loop
+          autoPlay
           playsInline
           preload="auto"
           className="pointer-events-none fixed inset-0 -z-10 h-full w-full object-cover"
